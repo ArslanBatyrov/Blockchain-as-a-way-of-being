68c43174d006ff0a9766047b9d0700023a6d3301
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-05-11 15:30:53 +0100

    Finalised my first proje ct: Guessing game. Added a loop to let a user to keep playing till they guess a number. More over, fixed the issue with quick crashing due to non-numerical input using error-handling(match)

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,35 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number! Let me give you a hint...");
+    println!("Guess the number! Let me give you a hint: between 1-100 (Inclusive)");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    let x = 7;
-    let y = 2;
-    println!("x = {x} and y + 2 = {}", y + 2);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new(); // Mutability is amazing
+        let mut guess = String::new(); // Mutability is amazing
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, //For the sake of making the game less fragile in term os a crash due to the non-numerical input
+        };
 
-    println!("You guessed: {guess}"); 
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small machine, try again!"),
-        Ordering::Greater => println!("Too big, try again!"),
-        Ordering::Equal => println!("You win! Well Done!"),
-    }
+        println!("You guessed: {guess}"); 
 
-   // println!("secret number is {secret_number}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small machine, try again!"),
+            Ordering::Greater => println!("Too big, try again!"),
+            Ordering::Equal => {
+                println!("You win! Well Done!");
+                break;  //Done to stop the game after a correct number is guessed
+            }
+        }
+}
+
 }
