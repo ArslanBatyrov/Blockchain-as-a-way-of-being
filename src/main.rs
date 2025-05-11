7471883e740fc0c1a3b917503bd7ff8173953acd
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-05-11 14:29:21 +0100

    utilised rand dependancy, read its docs. Generated a random number and printed it for a user

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
     println!("Guess the number! Let me give you a hint...");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     let x = 7;
     let y = 2;
     println!("x = {x} and y + 2 = {}", y + 2);
-
 
     println!("Please input your guess.");
 
@@ -17,4 +19,6 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}"); 
+    
+    println!("secret number is {secret_number}");
 }
