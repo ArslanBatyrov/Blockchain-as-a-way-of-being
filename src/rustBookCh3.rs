3becc317430f72f61a99b7e7afd0b96727a638e6
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-05-13 22:47:26 +0100

    Added notes explaining what is going on in rustBookCh3.rs to improve readability.

--- src/rustBookCh3.rs
+++ src/rustBookCh3.rs
@@ -1,8 +1,10 @@
 // Here I will be working with Chapter 3
 
+// Variables and Mutability
+
 fn main() {
     let x = 5;
-    println!("The value of x is: {x}");
+    println!("The value of x is {x}");
     x = 6;
-    println!("The value of x is: {x}");
-} 
+    println!("The value of x is {x}");
+}  // This is just a little snipped that throws an error as the code is immutable by default. Interesting
