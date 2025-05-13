1353dd1281d497c07f0ddcb9bb10648125cf1f28
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-05-13 23:07:52 +0100

    the Variables and Mutability part is finished. Covered Shadowing and its uses. Moving towards diving deeper into Data Types/Structures

--- src/rustBookCh3.rs
+++ src/rustBookCh3.rs
@@ -13,12 +13,23 @@
 //     println!("The value of x is {x}");
 // }  
  
- // Next I will play with muting the variable
+ // Next I made a variable mutable:
 
  fn main() {
+    const FORTY_FOR_TIMES_FOUR: u32 = 44 * 4;
     let mut x = 5;
     println!("The value of x is {x}");
     x = 18;
     println!("The value of x is {x}");
+    // Next lines of code are th epractice of shadowing. 
+    // This is useful when you want to change the type of the data stores(if required)
+    let m = 12;
+    let m = m -9;
+    {
+        let m = m * 2;
+        println!("The value of m in the inner scope is: {m}");
+    }
  }
+
+
 
