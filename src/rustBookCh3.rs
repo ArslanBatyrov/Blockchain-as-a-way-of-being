b4c2c5698ae31106c0528c9cbecab73d2cdd4134
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-05-13 22:56:58 +0100

    made the x variable mutable so that I can change the value of the variable further and fixed little syntax issues to avoid errors

--- src/rustBookCh3.rs
+++ src/rustBookCh3.rs
@@ -1,10 +1,24 @@
 // Here I will be working with Chapter 3
 
+//---------------
+
 // Variables and Mutability
 
-fn main() {
-    let x = 5;
+// This is just a little snipped that throws an error as the code is immutable by default. Interesting
+
+// fn main() {
+//     let x = 5;
+//     println!("The value of x is {x}");
+//     x = 6;
+//     println!("The value of x is {x}");
+// }  
+ 
+ // Next I will play with muting the variable
+
+ fn main() {
+    let mut x = 5;
     println!("The value of x is {x}");
-    x = 6;
+    x = 18;
     println!("The value of x is {x}");
-}  // This is just a little snipped that throws an error as the code is immutable by default. Interesting
+ }
+
