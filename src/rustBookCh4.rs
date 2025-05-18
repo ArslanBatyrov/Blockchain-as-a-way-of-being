eb40dfbe795f79c3c7ef130ad00aee61cc1f842c
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-05-18 22:11:54 +0100

    started learning the owners hip, basic concepts like garbage collector, security. Ch4

--- src/rustBookCh4.rs
+++ src/rustBookCh4.rs
@@ -1,1 +1,10 @@
 // Here I will be covering ownership
+// One of the most unique features.
+
+
+
+       {                    // s is not valid here, it’s not yet declared
+        let s = "hello";   // s is valid from this point forward
+
+        // do stuff with s
+               }           // this scope is now over, and s is no longer valid
