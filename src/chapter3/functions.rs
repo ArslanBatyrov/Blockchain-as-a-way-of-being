535ad391217c55c47f64579e126181f9665f6851
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-05-21 13:53:34 +0100

    functions in ch3  are fully covered and what I have learnt from it is added

--- src/chapter3/functions.rs
+++ src/chapter3/functions.rs
@@ -1,3 +1,31 @@
 fn main(){
+    println!("Hello Function World!");
 
+    // first_function(5, "Arslan");
+    let one_eight = second_function();
+    println!("{}", one_eight)
+}
+
+                            //First simple function
+// fn first_function(){
+//     let x = "Functions";
+
+//     println!("{}", x);
+// }
+
+                    // Parameters with a funciton;
+
+// fn first_function(value: i64, person: &str) {
+//     println!("The world is beautiful for number: {value} and {person} ");
+// }
+
+
+// New info: difference between statement and expression. Statement executes, return no value while 
+//an Expression evaluate to a resultant value
+
+
+                // Next will come a funciton that return a value
+
+fn second_function() -> u32 {
+    18
 }
