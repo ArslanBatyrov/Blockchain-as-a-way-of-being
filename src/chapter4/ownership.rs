55dbdfa2c6067afe48abbd0b926c0024a464464e
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-05-27 19:07:51 +0100

    explanation of slow pace of ch4

--- src/chapter4/ownership.rs
+++ src/chapter4/ownership.rs
@@ -1,1 +1,3 @@
 // ownership is uniquely Rust concept. Every single varibale owns the specific assigned data, this helps to get rid of the garbarge collector
+
+//Tomorrow I start doing the 2 hours every day of rust, for now were not able to focus fully on it as I had my Final Exams at uni
