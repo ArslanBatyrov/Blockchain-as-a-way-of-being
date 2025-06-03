c02397818ac7a6c94e3802ebfd374401164b9195
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-06-03 14:52:21 +0100

    Covered data types quickly, specifically vectors as macros

--- src/SolanaXEncode/week1.rs
+++ src/SolanaXEncode/week1.rs
@@ -1,2 +1,23 @@
 // Zero knowldge proofs are usually based on Rust
 
+
+
+// Structs: it is a unique data type you create
+
+struct Person: {
+    name: String,
+    age: u32,
+    email: String,
+
+}  //You create new data type, so that in the future 
+   //you can use it when required in the future
+
+   fn main(){
+    let mut alice = Person {name: "Arslan", age:20};
+
+   } // the way to initialise struct
+
+
+   //Vectors: Vectors give you moore flexibility that arrays
+// if you see exclamation mark = it is macro
+let names = vec!["Holy", "Alissa", "Arslan"];
