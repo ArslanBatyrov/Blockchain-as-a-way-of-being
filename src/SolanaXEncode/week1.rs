b054fd716cd810ea2a16463e668d84391297e14c
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-06-03 15:01:32 +0100

    enums, functions and details of usage of these are covered

--- src/SolanaXEncode/week1.rs
+++ src/SolanaXEncode/week1.rs
@@ -21,3 +21,45 @@ struct Person: {
    //Vectors: Vectors give you moore flexibility that arrays
 // if you see exclamation mark = it is macro
 let names = vec!["Holy", "Alissa", "Arslan"];
+
+
+// Next Arrays:
+
+// Arrays work in the same way as in another languages. 
+// Fature of arrays in rust is that they are static. Once created you cannot change the length
+// Can't add or remove items
+
+let variable_name = [value1, value2, value3];
+
+//You can't change the length but you can change the items withign the array
+
+
+// Next: What is a slice?
+
+//give flexibility os length is not specified at compile time
+
+// use 'as' keyword to change one data type into the other explicitly
+
+let a = 12;
+let b = a as string; 
+
+// What are Enums?
+
+enum Fruit {
+    Apple,
+    Orange,
+    Grape
+}
+
+//Referencing the enum :
+
+Fruit::Orange;
+
+//Functions in Rust
+
+fn my_func(a: u32) -> bool {
+    if a ==0 {
+        return false;
+    }
+    a == 7
+}
