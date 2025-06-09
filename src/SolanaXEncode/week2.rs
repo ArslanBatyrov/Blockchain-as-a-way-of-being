e7f07e2c94df212e9d5027e931dead280da0e210
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-06-09 18:29:44 +0500

    covered implementations, traits and details of accounts in Solana

--- src/SolanaXEncode/week2.rs
+++ src/SolanaXEncode/week2.rs
@@ -8,3 +8,47 @@ pub fn process_instruction(
 ) {
    
 }
+
+//----------
+// TO LEARN POST LESSON:
+// Learn the nuacnes of serialisation and deserialisation
+
+
+//-----------
+
+//traits help to specify the behaviour
+
+trait Details {
+    fn get_owner(&self) -> &Pubkey;
+    // fn get_admin(&self) --. etc
+}
+
+// implementations
+
+imp Details for AccountA {
+    gn get_owner(&self) -> &Pubkey {
+        &self.owner 
+    }
+}
+
+
+// Lesson 5 begins of real week 2: 
+
+// within an account there wil be number of keys used: key, isSigner, isWritable, rent_epoch
+
+// Owner versos holder
+
+// holder- holds the private key of the account!!!
+// It is holder who is allowed to transfer money
+// owner can ammend the data of any account
+
+// This is different from Ethereum.
+
+// There are 2 data accounts: System owned account and PDA(Program Derived Accounts)
+
+
+
+
+
+
+
