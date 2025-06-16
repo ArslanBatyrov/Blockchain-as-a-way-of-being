c01d544ea4ec712ddcb5853a44e161ff1b94f4b4
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-06-16 15:26:03 +0500

    revised structs form the solana bootcamp

--- src/SolanaXEncode/week1hw.rs
+++ src/SolanaXEncode/week1hw.rs
@@ -1,10 +1,32 @@
 //Here I will be revising the enetire week 1
 
-//tuples:
-fn main (){
-    let pair: (i32, &str) = (100, "Rust");
-    let (num, text) = pair;
-    println!("Number: {}", pair.0);
-    println!("Text: {}", pair.1);
+// tuples Revision:
+// fn main (){
+//     let pair: (i32, &str) = (100, "Rust");
+//     let (num, text) = pair;
+//     println!("Number: {}", pair.0);
+//     println!("Text: {}", pair.1);
+// }
+
+
+
+// Next: Structs Revision
+
+struct Man {
+    height: u64,
+    name: String,
+    age: u64,
 }
 
+
+impl Man {
+    fn birthday(&mut self){
+      self.age += 1;
+      println!("Happy {}th birthday, {}!", self.age, self.name);  
+    }
+}
+
+fn main() {
+    let mut arslan = Man { name: "Arslan".into(), age: 20, height: 188,};
+    arslan.birthday();
+}
