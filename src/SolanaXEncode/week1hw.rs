dd521719295a27b43e3123fdb013010d2390c088
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-06-16 15:11:40 +0500

    finally got proper acces to the internet, begin catching up woth the bootcamp

---
+++ src/SolanaXEncode/week1hw.rs
@@ -0,0 +1,10 @@
+//Here I will be revising the enetire week 1
+
+//tuples:
+fn main (){
+    let pair: (i32, &str) = (100, "Rust");
+    let (num, text) = pair;
+    println!("Number: {}", pair.0);
+    println!("Text: {}", pair.1);
+}
+
---
+++ src/SolanaXEncode/week2hw.rs
@@ -0,0 +1,1 @@
+// Here i will be revising the entire week 2
