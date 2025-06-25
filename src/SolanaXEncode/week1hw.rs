be8ce5b73643a4409f3ad909f445cc1486bd4b43
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-06-25 14:59:55 +0500

    solve two sums with rust, finished exercise for lesson 3

--- src/SolanaXEncode/week1hw.rs
+++ src/SolanaXEncode/week1hw.rs
@@ -408,18 +408,40 @@
 
 // version 2:
 
-fn main() {
+// fn main() {
 
-    let mut array = [1, 2, 3, 4,];
+//     let mut array = [1, 2, 3, 4,];
 
-    for i in 0..array.len(){
-        let temp = array[i];
-        array[i] = array[array.len() - i - 1];
-        array[array.len() - i - 1] = temp;
+//     for i in 0..array.len(){
+//         let temp = array[i];
+//         array[i] = array[array.len() - i - 1];
+//         array[array.len() - i - 1] = temp;
 
+//     }
+    
+//     // add your code here 
+//     println!("{:?}", array);
+    
+// }
+
+
+// Next: Two Sum Exercise with Rust
+
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+
+    for i in 0..nums.len(){
+
+         for j in (i+1)..nums.len(){
+            if nums[i] + nums[j] == target {
+               return vec![i as i32,j as i32] 
+            }
+        }
     }
-    
-    // add your code here 
-    println!("{:?}", array);
-    
+
+   vec![]
+}
+
+fn main() {  
+
+    println!("{:?}", two_sum(vec![2, 3, 4, 5,], 9));  
 }
