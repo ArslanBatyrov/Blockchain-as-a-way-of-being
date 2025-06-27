057e61c2dbc95a390274d1236b0395e846fb67fd
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-06-27 16:43:25 +0500

    solved funcitons.rs 4, rustlings

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -37,12 +37,38 @@
 
 // I AM NOT DONE
 
+// fn main() {
+//     call_this(5);
+// }
+
+// fn call_this(num: u32) {
+//     for i in 0..num {
+//         println!("Loop now {}", i + 1);
+//     }
+// }  //you just need to put a parameter into the funciton if you want it to compile(u32)
+
+
+// NEXT: functions4.rs
+// Make me compile! Execute `rustlings hint functions4` for hints :)
+
+// This store is having a sale where if the price is an even number, you get
+// 10 Rustbucks off, but if it's an odd number, it's 3 Rustbucks off.
+
+// I AM NOT DONE
+
 fn main() {
-    call_this(5);
+    let original_price = 51;
+    println!("Your sale price is {}", sale_price(original_price));
 }
 
-fn call_this(num: u32) {
-    for i in 0..num {
-        println!("Loop now {}", i + 1);
+fn sale_price(price: i32) -> i32 {
+    if is_even(price) {
+        price - 10
+    } else {
+        price - 3
     }
-}  //you just need to put a parameter into the funciton if you want it to compile(u32)
+}
+
+fn is_even(num: i32) -> bool {
+    num % 2 == 0
+}  //after the arrow -> you must specify the type you want to return
