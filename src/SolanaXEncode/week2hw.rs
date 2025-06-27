36f6bb9b1e52a59497f2cb88c377ec9d26a372d9
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-06-27 16:41:28 +0500

    functions.rs 3 is solved, rustlings, SolanaXEncode

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -5,10 +5,44 @@
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
 // I AM NOT DONE
-fn call_me() {
-    println!("Call me!");
-}
+// fn call_me() {
+//     println!("Call me!");
+// }
+
+// fn main() {
+//     call_me();
+// }
+
+
+// functions2.rs
+// Make me compile! Execute `rustlings hint functions2` for hints :)
+
+// I AM NOT DONE
+
+// fn main() {
+//     call_this(3);
+// }
+
+// fn call_this(num:i32) {
+//     for i in 0..num {
+//         println!("Loop! number {}", i + 1);
+//     }
+// }
+
+
+// Next: functions3.rs
+
+// functions3.rs
+// Make me compile! Execute `rustlings hint functions3` for hints :)
+
+// I AM NOT DONE
 
 fn main() {
-    call_me();
+    call_this(5);
 }
+
+fn call_this(num: u32) {
+    for i in 0..num {
+        println!("Loop now {}", i + 1);
+    }
+}  //you just need to put a parameter into the funciton if you want it to compile(u32)
