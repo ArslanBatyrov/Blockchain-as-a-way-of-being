2dc8418f5358da271da636c91d6f310d7308f28a
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-06-30 13:32:27 +0500

    all if rutlings given by encode are solved

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -96,13 +96,54 @@
 
 // I AM NOT DONE
 
-pub fn bigger(a: i32, b: i32) -> i32 {
+// pub fn bigger(a: i32, b: i32) -> i32 {
 
-    if a > b {
-        a
-    } else {
-            b
-        }
-}
+//     if a > b {
+//         a
+//     } else {
+//             b
+//         }
+// }
   
+// fn main() {}
+
+
+// if2.rs
+
+// Step 1: Make me compile!
+// Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
+// Execute the command `rustlings hint if2` if you want a hint :)
+
+// I AM NOT DONE
+
 fn main() {}
+
+pub fn fizz_if_foo(fizzish: &str) -> &str {
+    if fizzish == "fizz" {
+        "foo"
+    } 
+    else {
+        "1"
+    }
+}
+
+// No test changes needed!
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn foo_for_fizz() {
+        assert_eq!(fizz_if_foo("fizz"), "foo")
+    }
+
+    #[test]
+    fn bar_for_fuzz() {
+        assert_eq!(fizz_if_foo("fuzz"), "bar")
+    }
+
+    #[test]
+    fn default_to_baz() {
+        assert_eq!(fizz_if_foo("literally anything"), "baz")
+    }
+}
