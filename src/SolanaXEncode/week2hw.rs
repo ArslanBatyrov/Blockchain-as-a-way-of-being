94b0781e7793c51be4f27351c33f23a908d7cbe9
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-06-30 14:28:11 +0500

    solved all exercise 5 folder, finished variables6. Added every solution of mine

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -381,9 +381,20 @@
 
 // I AM NOT DONE
 
+// fn main() {
+//     let number = "T-H-R-E-E"; // don't change this line
+//     println!("Spell a Number : {}", number);
+//     let number = 3; // don't rename this variable
+//     println!("Number plus two is : {}", number + 2);
+// }
+
+
+// variables6.rs
+// Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
+
+// I AM NOT DONE
+
+const NUMBER: u32 = 3;
 fn main() {
-    let number = "T-H-R-E-E"; // don't change this line
-    println!("Spell a Number : {}", number);
-    let number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    println!("Number {}", NUMBER);
 }
