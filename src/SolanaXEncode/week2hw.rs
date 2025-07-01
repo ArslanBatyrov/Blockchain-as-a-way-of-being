ca2bc60dac7752cdd5cf5fe540405b5a494c505d
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-07-01 11:33:51 +0500

    vectors2.rs solved, finished the folder collections from the EncodeXSolana, will now move to questions ith enums

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -554,20 +554,58 @@
 
 // I AM NOT DONE
 
-fn array_and_vec() -> ([i32; 4], Vec<i32>) {
-    let a = [10, 20, 30, 40]; // a plain array
-    let v = vec![10,20,30,40];// TODO: declare your vector here with the macro for vectors
+// fn array_and_vec() -> ([i32; 4], Vec<i32>) {
+//     let a = [10, 20, 30, 40]; // a plain array
+//     let v = vec![10,20,30,40];// TODO: declare your vector here with the macro for vectors
 
-    (a, v)
+//     (a, v)
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_array_and_vec_similarity() {
+//         let (a, v) = array_and_vec();
+//         assert_eq!(a, v[..]);
+//     }
+// }
+
+// vec2.rs
+// A Vec of even numbers is given. Your task is to complete the loop
+// so that each number in the Vec is multiplied by 2.
+//
+// Make me pass the test!
+//
+// Execute the command `rustlings hint vec2` if you need
+// hints.
+
+// I AM NOT DONE
+
+fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
+    for i in v.iter_mut() {
+        *i = *i * 2; 
+    }
+
+    
+    println!("{:?}", v);
+
+    v  
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_array_and_vec_similarity() {
-        let (a, v) = array_and_vec();
-        assert_eq!(a, v[..]);
+    fn test_vec_loop() {
+        let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
+        let ans = vec_loop(v.clone());
+
+        assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
     }
 }
+
+fn main(){}
