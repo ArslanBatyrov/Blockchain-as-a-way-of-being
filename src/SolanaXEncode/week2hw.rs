bba9eda6d20106d789d42a20fb5062e4746eb885
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-07-02 10:51:47 +0500

    generics3.rs is solved

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -924,13 +924,61 @@
 
 // I AM NOT DONE
 
-struct Wrapper<T> {
-    value: T,
+// struct Wrapper<T> {
+//     value: T,
+// }
+
+// impl<T> Wrapper<T> {
+//     pub fn new(value: T) -> Self {
+//         Wrapper { value }
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn store_u32_in_wrapper() {
+//         assert_eq!(Wrapper::new(42).value, 42);
+//     }
+
+//     #[test]
+//     fn store_str_in_wrapper() {
+//         assert_eq!(Wrapper::new("Foo").value, "Foo");
+//     }
+// }
+
+// fn main(){}
+
+
+// ------------------------------------
+
+// An imaginary magical school has a new report card generation system written in Rust!
+// Currently the system only supports creating report cards where the student's grade
+// is represented numerically (e.g. 1.0 -> 5.5).
+// However, the school also issues alphabetical grades (A+ -> F-) and needs
+// to be able to print both types of report card!
+
+// Make the necessary code changes in the struct ReportCard and the impl block
+// to support alphabetical report cards. Change the Grade in the second test to "A+"
+// to show that your changes allow alphabetical grades.
+
+// Execute 'rustlings hint generics3' for hints!
+
+// I AM NOT DONE
+
+use std::fmt::Display;
+pub struct ReportCard<T,E,A> {
+    pub grade: T,
+    pub student_name: E,
+    pub student_age: A,
 }
 
-impl<T> Wrapper<T> {
-    pub fn new(value: T) -> Self {
-        Wrapper { value }
+impl<T: Display, E: Display,A: Display> ReportCard<T,E,A> {
+    pub fn print(&self) -> String {
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade)
     }
 }
 
@@ -939,14 +987,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn store_u32_in_wrapper() {
-        assert_eq!(Wrapper::new(42).value, 42);
+    fn generate_numeric_report_card() {
+        let report_card = ReportCard {
+            grade: 2.1,
+            student_name: "Tom Wriggle".to_string(),
+            student_age: 12,
+        };
+        assert_eq!(
+            report_card.print(),
+            "Tom Wriggle (12) - achieved a grade of 2.1"
+        );
     }
 
     #[test]
-    fn store_str_in_wrapper() {
-        assert_eq!(Wrapper::new("Foo").value, "Foo");
+    fn generate_alphabetic_report_card() {
+        // TODO: Make sure to change the grade here after you finish the exercise.
+        let report_card = ReportCard {
+            grade: 2.1,
+            student_name: "Gary Plotter".to_string(),
+            student_age: 11,
+        };
+        assert_eq!(
+            report_card.print(),
+            "Gary Plotter (11) - achieved a grade of A+"
+        );
     }
 }
-
-fn main(){}
