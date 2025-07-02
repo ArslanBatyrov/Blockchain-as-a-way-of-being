29c64c247e996834a3fa8457c9a99d0be5a71010
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-07-02 11:06:35 +0500

    move_semantics1 and 2 are solved and added

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -968,48 +968,63 @@
 
 // I AM NOT DONE
 
-use std::fmt::Display;
-pub struct ReportCard<T,E,A> {
-    pub grade: T,
-    pub student_name: E,
-    pub student_age: A,
+
+//----------------------------
+
+// NEXT: Move Semantics
+
+// move_semantics1.rs
+// Make me compile! Execute `rustlings hint move_semantics1` for hints :)
+
+// I AM NOT DONE
+
+// fn main() {
+//     let vec0 = Vec::new();
+
+//     let mut vec1 = fill_vec(vec0);
+
+//     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+
+//     vec1.push(88);
+
+//     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+// }
+
+// fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+//     let mut vec = vec;
+
+//     vec.push(22);
+//     vec.push(44);
+//     vec.push(66);
+
+//     vec
+// }
+
+// move_semantics2.rs
+// Make me compile without changing line 13 or moving line 10!
+// Execute `rustlings hint move_semantics2` for hints :)
+
+// I AM NOT DONE
+
+fn main() {
+    let vec0 = Vec::new();
+
+    let mut vec1 = fill_vec(&vec0);
+
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec1.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-impl<T: Display, E: Display,A: Display> ReportCard<T,E,A> {
-    pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
-    }
-}
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.clone();
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
 
-    #[test]
-    fn generate_numeric_report_card() {
-        let report_card = ReportCard {
-            grade: 2.1,
-            student_name: "Tom Wriggle".to_string(),
-            student_age: 12,
-        };
-        assert_eq!(
-            report_card.print(),
-            "Tom Wriggle (12) - achieved a grade of 2.1"
-        );
-    }
-
-    #[test]
-    fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
-            student_name: "Gary Plotter".to_string(),
-            student_age: 11,
-        };
-        assert_eq!(
-            report_card.print(),
-            "Gary Plotter (11) - achieved a grade of A+"
-        );
-    }
+    vec
 }
