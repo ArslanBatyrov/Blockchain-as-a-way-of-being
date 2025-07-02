d4c7e0d30dd494e6f4aa4750a682363931970f13
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-07-02 11:39:02 +0500

    all move semantics rustlings  are solved

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -1095,11 +1095,38 @@
 
 // I AM NOT DONE
 
+// fn main() {
+//     let mut x = 100;
+//     let y = &mut x;
+//     *y += 100;
+//     let z = &mut x;
+//     *z += 1000;
+//     assert_eq!(x, 1200);
+// }
+
+
+// move_semantics6.rs
+// Make me compile! `rustlings hint move_semantics6` for hints
+// You can't change anything except adding or removing references
+
+// I AM NOT DONE
+
 fn main() {
-    let mut x = 100;
-    let y = &mut x;
-    *y += 100;
-    let z = &mut x;
-    *z += 1000;
-    assert_eq!(x, 1200);
+    let data = "Rust is great!".to_string();
+
+    get_char(&data);
+
+    string_uppercase(data);
+}
+
+// Should not take ownership
+fn get_char(data: &String) -> char {
+    data.chars().last().unwrap()
+}
+
+// Should take ownership
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
+
+    println!("{}", data);
 }
