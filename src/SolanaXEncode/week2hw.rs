286685c8b51b183e5cd28cac7fa5ed0c72eee2c4
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-07-02 11:59:04 +0500

    completed homework 6 from SolanaXEncode bootcamp, finally. Moving to the folder 7

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -1166,23 +1166,45 @@
 
 // I AM NOT DONE
 
+// fn main() {
+//     let optional_word = Some(String::from("rustlings"));
+//     // TODO: Make this an if let statement whose value is "Some" type
+//     if let Some(word) = optional_word {
+//         println!("The word is: {}", word);
+//     } else {
+//         println!("The optional word doesn't contain anything");
+//     }
+
+//     let mut optional_integers_vec: Vec<Option<i8>> = Vec::new();
+//     for x in 1..10 {
+//         optional_integers_vec.push(Some(x));
+//     }
+
+//     // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
+//     // You can stack `Option<T>`'s into while let and if let
+//     while let Some(Some(integer)) = optional_integers_vec.pop() {
+//         println!("current value: {}", integer);
+//     }
+// }
+
+
+
+// option3.rs
+// Make me compile! Execute `rustlings hint option3` for hints
+
+// I AM NOT DONE
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
-    let optional_word = Some(String::from("rustlings"));
-    // TODO: Make this an if let statement whose value is "Some" type
-    if let Some(word) = optional_word {
-        println!("The word is: {}", word);
-    } else {
-        println!("The optional word doesn't contain anything");
-    }
+    let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    let mut optional_integers_vec: Vec<Option<i8>> = Vec::new();
-    for x in 1..10 {
-        optional_integers_vec.push(Some(x));
+    match &y {
+        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        _ => println!("no match"),
     }
-
-    // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
-    // You can stack `Option<T>`'s into while let and if let
-    while let Some(Some(integer)) = optional_integers_vec.pop() {
-        println!("current value: {}", integer);
-    }
+    y; // Fix without deleting this line.
 }
