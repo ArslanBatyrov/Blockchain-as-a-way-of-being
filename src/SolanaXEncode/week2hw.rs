f438fc3a14408d1a19d437d85753291e91602bce
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-07-03 16:28:56 +0500

    errors3.rs is solved

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -1380,34 +1380,120 @@
 
 // I AM NOT DONE
 
-pub fn generate_nametag_text(name: String) -> Result<String, String> {
-    if name.len() > 0 {
-        Ok(format!("Hi! My name is {}", name))
+// pub fn generate_nametag_text(name: String) -> Result<String, String> {
+//     if name.len() > 0 {
+//         Ok(format!("Hi! My name is {}", name))
+//     } else {
+//         Err("`name` was empty; it must be nonempty.".into())
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn generates_nametag_text_for_a_nonempty_name() {
+//         assert_eq!(
+//             generate_nametag_text("Beyoncé".into()),
+//             Ok("Hi! My name is Beyoncé".into())
+//         );
+//     }
+
+//     #[test]
+//     fn explains_why_generating_nametag_text_fails() {
+//         assert_eq!(
+//             generate_nametag_text("".into()),
+//             // Don't change this line
+//             Err("`name` was empty; it must be nonempty.".into())
+//         );
+//     }
+// }
+
+// fn main(){}
+
+
+// errors2.rs
+// Say we're writing a game where you can buy items with tokens. All items cost
+// 5 tokens, and whenever you purchase items there is a processing fee of 1
+// token. A player of the game will type in how many items they want to buy,
+// and the `total_cost` function will calculate the total number of tokens.
+// Since the player typed in the quantity, though, we get it as a string-- and
+// they might have typed anything, not just numbers!
+
+// Right now, this function isn't handling the error case at all (and isn't
+// handling the success case properly either). What we want to do is:
+// if we call the `parse` function on a string that is not a number, that
+// function will return a `ParseIntError`, and in that case, we want to
+// immediately return that error from our function and not try to multiply
+// and add.
+
+// There are at least two ways to implement this that are both correct-- but
+// one is a lot shorter! Execute `rustlings hint errors2` for hints to both ways.
+
+// I AM NOT DONE
+
+// use std::num::ParseIntError;
+
+// pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+//     let processing_fee = 1;
+//     let cost_per_item = 5;
+//     let qty = item_quantity.parse::<i32>()?;
+
+//     Ok(qty * cost_per_item + processing_fee)
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn item_quantity_is_a_valid_number() {
+//         assert_eq!(total_cost("34"), Ok(171));
+//     }
+
+//     #[test]
+//     fn item_quantity_is_an_invalid_number() {
+//         assert_eq!(
+//             total_cost("beep boop").unwrap_err().to_string(),
+//             "invalid digit found in string"
+//         );
+//     }
+// }
+
+
+// fn main(){}
+
+// errors3.rs
+// This is a program that is trying to use a completed version of the
+// `total_cost` function from the previous exercise. It's not working though!
+// Why not? What should we do to fix it?
+// Execute `rustlings hint errors3` for hints!
+
+// I AM NOT DONE
+
+use std::num::ParseIntError;
+
+fn main() -> Result<(), std::num::ParseIntError> {
+    let mut tokens = 100;
+    let pretend_user_input = "8";
+
+    let cost = total_cost(pretend_user_input)?;
+
+    if cost > tokens {
+        println!("You can't afford that many!");
     } else {
-        Err("`name` was empty; it must be nonempty.".into())
+        tokens -= cost;
+        println!("You now have {} tokens.", tokens);
     }
+
+     Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+    let processing_fee = 1;
+    let cost_per_item = 5;
+    let qty = item_quantity.parse::<i32>()?;
 
-    #[test]
-    fn generates_nametag_text_for_a_nonempty_name() {
-        assert_eq!(
-            generate_nametag_text("Beyoncé".into()),
-            Ok("Hi! My name is Beyoncé".into())
-        );
-    }
-
-    #[test]
-    fn explains_why_generating_nametag_text_fails() {
-        assert_eq!(
-            generate_nametag_text("".into()),
-            // Don't change this line
-            Err("`name` was empty; it must be nonempty.".into())
-        );
-    }
+    Ok(qty * cost_per_item + processing_fee)
 }
-
-fn main(){}
