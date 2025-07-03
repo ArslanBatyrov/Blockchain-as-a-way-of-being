81e31d73eeea7f6391c4f34ca336d571fa3a629b
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-07-03 13:30:56 +0500

    started error_handling rustlings, realised I am not proficient in error handling. Will go though chapter 9 Rustbook to get better first

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -1215,17 +1215,64 @@
 //Given a vector nums, create a new vector called runningSum where each element at index i is the sum of all elements from the beginning of the vector up to index i.
 //Return the running sum of nums.
 
-fn running_sum(nums: Vec<i32>) -> Vec<i32> { 
-    let mut new_vector:Vec<i32> = Vec::with_capacity(nums.len()); 
-    let mut sum = 0;
+// fn running_sum(nums: Vec<i32>) -> Vec<i32> { 
+//     let mut new_vector:Vec<i32> = Vec::with_capacity(nums.len()); 
+//     let mut sum = 0;
 
-    for i in nums{
-        sum += i;
-        new_vector.push(sum);
-    }
-    new_vector
-}  
+//     f or i in nums{
+//         sum += i;
+//         new_vector.push(sum);
+//     }
+//     new_vector
+// }  
   
-fn main() {  
-    println!("{:?}", running_sum(vec![1, 1, 1, 1, 1]));
+// fn main() {  
+//     println!("{:?}", running_sum(vec![1, 1, 1, 1, 1]));
+// }
+
+
+//------------
+// Next: homework7, last rust homeworks
+// Error_handling
+
+// errors1.rs
+// This function refuses to generate text to be printed on a nametag if
+// you pass it an empty string. It'd be nicer if it explained what the problem
+// was, instead of just sometimes returning `None`. Thankfully, Rust has a similar
+// construct to `Option` that can be used to express error conditions. Let's use it!
+// Execute `rustlings hint errors1` for hints!
+
+// I AM NOT DONE
+
+pub fn generate_nametag_text(name: String) -> Option<String> {
+    if name.len() > 0 {
+        Some(format!("Hi! My name is {}", name))
+    } else {
+        // Empty names aren't allowed.
+        None
+    }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generates_nametag_text_for_a_nonempty_name() {
+        assert_eq!(
+            generate_nametag_text("Beyoncé".into()),
+            Ok("Hi! My name is Beyoncé".into())
+        );
+    }
+
+    #[test]
+    fn explains_why_generating_nametag_text_fails() {
+        assert_eq!(
+            generate_nametag_text("".into()),
+            // Don't change this line
+            Err("`name` was empty; it must be nonempty.".into())
+        );
+    }
+}
+
+// I can't solve this because I do not understand error handling at all. Will go thorugh rust book now
