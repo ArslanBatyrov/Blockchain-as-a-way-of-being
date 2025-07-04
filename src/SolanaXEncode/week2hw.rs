4cea67c3e7a2e515eb7f675aa1b77134b962ca2f
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-07-04 14:14:19 +0500

    solved traits1.rs and added

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -1963,8 +1963,53 @@
 
 // I AM NOT DONE
 
-pub fn is_even(num: i32) -> bool {
-    num % 2 == 0
+// pub fn is_even(num: i32) -> bool {
+//     num % 2 == 0
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn is_true_when_even() {
+//         assert!(is_even(4));
+//     }
+
+//     #[test]
+//     fn is_false_when_odd() {
+//         assert!(!is_even(5));
+//     }
+// }
+
+
+// traits1.rs
+// Time to implement some traits!
+//
+// Your task is to implement the trait
+// `AppendBar' for the type `String'.
+//
+// The trait AppendBar has only one function,
+// which appends "Bar" to any object
+// implementing this trait.
+
+// I AM NOT DONE
+
+trait AppendBar {
+    fn append_bar(self) -> Self;
+}
+
+impl AppendBar for String {
+    fn append_bar(mut self) -> String {
+        self.push_str("Bar");
+        self
+    }
+}
+
+fn main() {
+    let s = String::from("Foo");
+    let s = s.append_bar();
+    println!("s: {}", s);
 }
 
 #[cfg(test)]
@@ -1972,12 +2017,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn is_true_when_even() {
-        assert!(is_even(4));
+    fn is_foo_bar() {
+        assert_eq!(String::from("Foo").append_bar(), String::from("FooBar"));
     }
 
     #[test]
-    fn is_false_when_odd() {
-        assert!(!is_even(5));
+    fn is_bar_bar() {
+        assert_eq!(
+            String::from("").append_bar().append_bar(),
+            String::from("BarBar")
+        );
     }
 }
