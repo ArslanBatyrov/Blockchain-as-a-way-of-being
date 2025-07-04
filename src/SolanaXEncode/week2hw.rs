2748589fd1fb4e02d4099cf7dd1e1319c0073b05
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-07-04 14:19:15 +0500

    solved traits2.rs, also finished all the homeworks from SolanaXEncode. Homework7 is done and added

--- src/SolanaXEncode/week2hw.rs
+++ src/SolanaXEncode/week2hw.rs
@@ -1995,21 +1995,67 @@
 
 // I AM NOT DONE
 
+// trait AppendBar {
+//     fn append_bar(self) -> Self;
+// }
+
+// impl AppendBar for String {
+//     fn append_bar(mut self) -> String {
+//         self.push_str("Bar");
+//         self
+//     }
+// }
+
+// fn main() {
+//     let s = String::from("Foo");
+//     let s = s.append_bar();
+//     println!("s: {}", s);
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn is_foo_bar() {
+//         assert_eq!(String::from("Foo").append_bar(), String::from("FooBar"));
+//     }
+
+//     #[test]
+//     fn is_bar_bar() {
+//         assert_eq!(
+//             String::from("").append_bar().append_bar(),
+//             String::from("BarBar")
+//         );
+//     }
+// }
+
+
+// traits2.rs
+//
+// Your task is to implement the trait
+// `AppendBar' for a vector of strings.
+//
+// To implement this trait, consider for
+// a moment what it means to 'append "Bar"'
+// to a vector of strings.
+//
+// No boiler plate code this time,
+// you can do this!
+
+// I AM NOT DONE
+
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-impl AppendBar for String {
-    fn append_bar(mut self) -> String {
-        self.push_str("Bar");
+impl AppendBar for Vec<String>{
+    fn append_bar(mut self) -> Vec<String> {
+        for s in &mut self {
+            s.push_str("Bar");
+        }
         self
     }
-}
-
-fn main() {
-    let s = String::from("Foo");
-    let s = s.append_bar();
-    println!("s: {}", s);
 }
 
 #[cfg(test)]
@@ -2017,15 +2063,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn is_foo_bar() {
-        assert_eq!(String::from("Foo").append_bar(), String::from("FooBar"));
-    }
-
-    #[test]
-    fn is_bar_bar() {
-        assert_eq!(
-            String::from("").append_bar().append_bar(),
-            String::from("BarBar")
-        );
+    fn is_vec_pop_eq_bar() {
+        let mut foo = vec![String::from("Foo")].append_bar();
+        assert_eq!(foo.pop().unwrap(), String::from("Bar"));
+        assert_eq!(foo.pop().unwrap(), String::from("Foo"));
     }
 }
