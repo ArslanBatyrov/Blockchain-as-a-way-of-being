e623e384d875df3efab76b3be5f03455c94bd5c2
ArslanBatyrov <batyrovarslan7@gmail.com>
2025-07-07 11:00:45 +0500

    I am trying to get better at using Anchor, this is week3, however Encode bootcamp explanation was not good enough. Thus I started going over 11 hours course for Solana Development in parallel. As I was going through the course, I realised that I need to get a deeper understanding of lifetime, so I will do that now here

---
+++ src/SolanaXEncode/week3hw.rs
@@ -0,0 +1,5 @@
+// Next: Lesson 9. I start my week 3 revision
+
+// I ahve realised that SolanaXEncode explanation of anchor is really bad
+// So I started watching the Solana Bootcamp 11hours video to get deeper understanding of the Anchor
+// As I was watching the video, I realised that I need to dive deeper into lifetimes
